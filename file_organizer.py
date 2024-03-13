541d942069d2a6b532e7534ea1780be414fd0dd5
Miguel Angel <miguelanghdez@outlook>
2024-03-13 14:28:18 +0000

    Optimized for a better performance and better visualization

--- file_organizer.py
+++ file_organizer.py
@@ -4,20 +4,15 @@
 
 import os
 import shutil
+import time
 
 class FileOrganizer:
-    def __init__(self, main_path, pdfs_path, inst_path, img_path, zip_path, excel_path, torrent_path, txt_path):
+    def __init__(self, main_path, folders):
         self.main_path = main_path
-        self.pdfs_path = pdfs_path
-        self.inst_path = inst_path
-        self.img_path = img_path
-        self.zip_path = zip_path
-        self.excel_path = excel_path
-        self.torrent_path = torrent_path
-        self.txt_path = txt_path
+        self.folders = folders
 
         # Crear las carpetas si no existen
-        for path in [self.pdfs_path, self.inst_path, self.img_path, self.zip_path, self.excel_path, self.torrent_path, self.txt_path]:
+        for path in self.folders.values():
             if not os.path.exists(path):
                 os.makedirs(path)
 
@@ -26,34 +21,31 @@ class FileOrganizer:
             with os.scandir(self.main_path) as entries:
                 for entry in entries:
                     if entry.is_file():
-                        if entry.name.endswith('.pdf'):
-                            shutil.move(entry.path, self.pdfs_path)
-                        elif entry.name.endswith('.exe'):
-                            shutil.move(entry.path, self.inst_path)
-                        elif entry.name.endswith(('.jpg', '.png', '.gif', '.jpeg', '.bmp')):
-                            shutil.move(entry.path, self.img_path)
-                        elif entry.name.endswith(('.zip', '.7z')):
-                            shutil.move(entry.path, self.zip_path)
-                        elif entry.name.endswith('.xlsx'):
-                            shutil.move(entry.path, self.excel_path)
-                        elif entry.name.endswith('.torrent'):
-                            shutil.move(entry.path, self.torrent_path)
-                        elif entry.name.endswith(('.txt', '.docx')):
-                            shutil.move(entry.path, self.txt_path)
+                        file_ext = os.path.splitext(entry.name)[1].lower()
+                        if file_ext in self.folders:
+                            shutil.move(entry.path, self.folders[file_ext])
             time.sleep(10)
 
 # Definir las rutas de las carpetas
 main_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads')
-pdfs_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads', 'PDFs')
-inst_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads', 'Instaladores')
-img_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads', 'Imagenes')
-zip_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads', 'Zips')
-excel_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads', 'Excels')
-torrent_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads', 'Torrents')
-txt_path = os.path.join('C:', os.sep, 'Users', 'migue', 'Downloads', 'Textos')
+folders = {
+    '.pdf': os.path.join(main_path, 'PDFs'),
+    '.exe': os.path.join(main_path, 'Instaladores'),
+    '.jpg': os.path.join(main_path, 'Imagenes'),
+    '.png': os.path.join(main_path, 'Imagenes'),
+    '.gif': os.path.join(main_path, 'Imagenes'),
+    '.jpeg': os.path.join(main_path, 'Imagenes'),
+    '.bmp': os.path.join(main_path, 'Imagenes'),
+    '.zip': os.path.join(main_path, 'Zips'),
+    '.7z': os.path.join(main_path, 'Zips'),
+    '.xlsx': os.path.join(main_path, 'Excels'),
+    '.torrent': os.path.join(main_path, 'Torrents'),
+    '.txt': os.path.join(main_path, 'Textos'),
+    '.docx': os.path.join(main_path, 'Textos')
+}
 
 # Crear una instancia de la clase FileOrganizer
-organizer = FileOrganizer(main_path, pdfs_path, inst_path, img_path, zip_path, excel_path, torrent_path, txt_path)
+organizer = FileOrganizer(main_path, folders)
 
 # Llamar al método move_files para comenzar a mover los archivos
 organizer.move_files()
